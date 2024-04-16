c8b0385823220024f834566bc51e7f7de78f0550
chisanga kabwe <chisangasage@gmail.com>
2024-04-16 12:26:37 +0200

    fix login redirect 2

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -22,7 +22,7 @@ def login_view(request):
             user = User.objects.authenticate(email=email, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('index')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
